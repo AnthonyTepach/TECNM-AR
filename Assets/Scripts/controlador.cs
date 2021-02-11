0134a7e430b3c40d63c6d7c419eb1a2c05937e88
Programador Computer Forms <developer@Cf.LAN>
2021-02-11 13:09:57 -0600

    Initial commit

---
+++ Assets/Scripts/controlador.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class controlador : MonoBehaviour
+{
+   public void nextScene(string nameScene){
+       SceneManager.LoadScene(nameScene);
+    }
+
+    public void exitAPP(){
+        Application.Quit();
+    }
+}
